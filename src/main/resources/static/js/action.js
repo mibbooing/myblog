6a8dd76759865a89a271908f3600f5b3b247064c
mibbooing <qkqhdi2426@gmail.com>
2022-12-01 11:43:52 +0900

    Feat: 레이아웃 분할, member 템플릿, home 템플릿 생성  - 레이아웃 분할(home, member, blog)  - member, home 레이아웃 fragment 및 템플릿 생성

--- src/main/resources/static/js/action.js
+++ src/main/resources/static/js/action.js
@@ -1,12 +1,14 @@
-$(document).ready(
-                    function(){
-                        $('#btn_menu').on('click', function(){
-                            $('.area-aside').addClass('action');
-                            $('.overlay').fadeIn();
-                        });
-                        $('.overlay').on('click', function(){
-                            $('.area-aside').removeClass('action');
-                            $('.overlay').fadeOut();
-                        });
-
-                });
+$(document).ready(function(){
+   $('#btn_menu').on('click', function(){
+      $('.area-aside').addClass('action');
+      $('.overlay').fadeIn();
+   });
+   $('.overlay').on('click', function(){
+      $('.area-aside').removeClass('action');
+      $('.overlay').fadeOut();
+   });
+   $('.btn_home_menu').on('click', function(){
+        console.log("!!");
+      $('.navbar_menu').toggleClass('active');
+   });
+});
